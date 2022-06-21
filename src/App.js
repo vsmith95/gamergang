f2568593033f9bb87121b36d281e3270ceebc857
vsmith95 <vaughnsmith95@gmail.com>
2022-06-21 17:23:07 -0400

    added some place holder components and set up app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import UserProfile from "./components/UserProfile";
+import LoLMain from "./components/LoLMain";
+import ApexMain from "./components/LoLMain";
+import Login from "./components/Login";
+
+// Team should exist in footer probably
+
+// import Team from "./components/Team";
+
+// sections that may be moved to respective components
+// import ApexHeros from './components/ApexHeros';
+// import ApexProfile from "./components/ApexProfile";
+// import ApexLB from "./components/ApexLB";
+// import LoLChamps from "./components/LoLChamps";
+// import LoLPofile from "./components/LoLProfile";
+// import LoLLB from "./components/LoLLB";
+
 
 function App() {
-  return (
+  const [pages] = useState([
+    {
+        name: "home",
+        display: "Home",
+        component: <Home />
+    },
+    {
+        name: "lolMain",
+        display: "League of Legends",
+        component: <LoLMain />
+    },
+    {
+        name: "apexMain",
+        display: "Apex Legends",
+        component: <ApexMain />
+    },
+    // Log in and Profile should be contingent on where the user is logged in or not
+    // if logged out, display log in
+    // if logged in, display profile
+    // we can handle this once we have a logged in state set up
+    {
+        name: "login",
+        display: "Log-in",
+        component: <Login />
+    },
+    {
+      name: "userProfile",
+      display: "Profile",
+      component: <UserProfile />
+    }
+]);
+
+const [currentPage, setCurrentPage] = useState(pages[0]);
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header
+        pages={pages}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        />
+        <main>
+            {currentPage.component}
+        </main>
+        <Footer />
     </div>
-  );
+);
+
 }
 
 export default App;
