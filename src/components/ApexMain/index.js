5014fad4e19c470bed8309ca37f203105529a99c
vsmith95 <vaughnsmith95@gmail.com>
2022-06-21 17:56:37 -0400

    final preproduction for dev branch

--- src/components/ApexMain/index.js
+++ src/components/ApexMain/index.js
@@ -1,3 +1,6 @@
 import React, {useState} from "react";
+import ApexHeros from './components/ApexHeros';
+import ApexProfile from "./components/ApexProfile";
+import ApexLB from "./components/ApexLB";
 
 export default 
--- src/components/LoLMain/index.js
+++ src/components/LoLMain/index.js
@@ -1,3 +1,7 @@
 import React, {useState} from "react";
+import LoLChamps from "./components/LoLChamps";
+import LoLPofile from "./components/LoLProfile";
+import LoLLB from "./components/LoLLB";
+
 
 export default 
