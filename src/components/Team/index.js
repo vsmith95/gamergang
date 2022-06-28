5d4b5be37765e7554615e3129cdd69d86ad03b95
kevnikica <kovnin1997@gmail.com>
2022-06-27 22:12:51 -0400

    more styling changes featuring vaughns face

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+import kevniPic from "../../assets/images/kevnipic.jpg"
+import vaughnPic from "../../assets/images/vaughnpic.png"
 
 const Team = () => {
     return (
         // meet the team container
         <section>
-            <div>
+            <div class="teamMembers">
                 {/* meet the team cards */}
-                <div>
+                <div class="clinton">
                     <div>
                         <h3>Clinton "Otafu" Tarzia</h3>
                     </div>
@@ -21,7 +23,7 @@ const Team = () => {
                     </div>
                 </div>
 
-                <div>
+                <div class="kevni">
                     <div>
                         <h3>Kevni "Koney" Kica</h3>
                     </div>
@@ -36,7 +38,7 @@ const Team = () => {
                     </div>
                 </div>
 
-                <div>
+                <div class="vaughn">
                     <div>
                         <h3>Vaughn "Veez" Smith</h3>
                     </div>
@@ -49,6 +51,17 @@ const Team = () => {
                     <div>
                         <p>Mini Bio?</p>
                     </div>
+                </div>
+            </div>
+            <div class="teamPics">
+                <div class="clintonPic">
+                    <img class="kevniPic" src={kevniPic} alt="" />
+                </div>
+                <div>
+                    <img class="kevniPic" src={kevniPic} alt="" />
+                </div>
+                <div class="vaughnPic">
+                    <img class="kevniPic" src={vaughnPic} alt="" />
                 </div>
             </div>
         </section>
