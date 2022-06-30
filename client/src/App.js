8e69957071556ae704ae54ce400f0cc05eb5075d
vsmith95 <vaughnsmith95@gmail.com>
2022-06-29 22:03:07 -0400

    fixed the api that we broke on a diff branch lol

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
-import Categories from "./components/Categories";
+import Search from "./components/Search";
 import Game from "./components/Game";
 import Team from "./components/Team";
 import { useAuth0 } from "@auth0/auth0-react"
@@ -15,16 +15,16 @@ const App = () => {
             display: "Home",
             component: <Home />
         },
-        // Remove "game" block once categories or other means of reaching the game page are enabled
+        // Remove "game" block once Search or other means of reaching the game page are enabled
         {
             name: "game",
             display: "Game",
             component: <Game />
         },
         {
-            name: "categories",
-            display: "Categories",
-            component: <Categories />
+            name: "search",
+            display: "Search",
+            component: <Search />
         },
         {
             name: "team",
