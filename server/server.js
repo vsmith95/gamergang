16f386df18de30af4c96ec82f27128d330ec7d34
vsmith95 <vaughnsmith95@gmail.com>
2022-06-30 20:12:46 -0400

    more heroku fixing

--- server/server.js
+++ server/server.js
@@ -94,5 +94,5 @@ app.use("/", (req, res) =>
   })
 );
 
-const port = process.env.port || 3002;
+const port = process.env.PORT || 3002;
 app.listen(port, () => console.log("Example app listening on port " + port));
