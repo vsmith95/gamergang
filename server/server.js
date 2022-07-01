bdf1de0c522a12747c9d87c23188d95fc135c678
vsmith95 <vaughnsmith95@gmail.com>
2022-06-30 22:49:48 -0400

    add path

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 var cors = require("cors");
+const path = require("path");
 const mongoose = require("mongoose");
 
 const instance = axios.create({
